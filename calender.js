8bf1db734d3bed545cf61b7a38b2c4a1445d1655
KENMIDAM <t.saitoh1994@gmail.com>
2022-09-13 23:50:31 +0900

    Update calender.js

--- calender.js
+++ calender.js
@@ -1,4 +1,4 @@
-const weeks =['日', '月', '火', '水', '木', '金', '土']
+const weeks =['日', '月', '火', '水', '木', '金', '土'];
 const date = new Date();
 const year =date.getFullyear();
 const month = date.getMonth() + 1;
@@ -8,8 +8,8 @@ const endDayCount = endDate.getDate();
 const startDay = startDate.getDay();
 
 let getCount = 1;
-let calenderHtml = ''
-;
+let calenderHtml = '';
+
 calenderHtml += '<h1>' + year + '/' + month + '</h1>';
 calenderHtml += '<table>';
 
