b437468b2a2ecd270809cc2d678b990363402d7d
KENMIDAM <t.saitoh1994@gmail.com>
2022-10-19 13:05:12 +0000

    Update time.js

--- time.js
+++ time.js
@@ -1,4 +1,4 @@
-const clock = () =>{
+function clock() {
     //現在日時取得
     const d = new Date();
 
@@ -6,30 +6,30 @@ const clock = () =>{
     let year = d.getFullYear();
     let month = d.getMonth();
     let date = d.getDate();
-    
+
     let dayNum = d.getDay();
-    const week = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
-    let day =  week[dayNum];
-    
+    const week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    let day = week[dayNum];
+
     let hour = d.getHours();
     let min = d.getMinutes();
     let sec = d.getSeconds();
 
     //日付の繰り上げ
-    month = month< 10 ? "0" + month : month;
-    date = date< 10 ? "0" + date : date;
-    hour = hour< 10 ? "0" + hour : hour;
-    min = min< 10 ? "0" + min : min;
-    sec = sec< 10 ? "0" + sec : sec;
+    month = month < 10 ? "0" + month : month;
+    date = date < 10 ? "0" + date : date;
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
 
     //文字列形成
     let today = `${year}.${month}.${date},${day}.`;
     let time = `${hour}:${min}:${sec}`;
-    
+
     //文字列出力
     document.querySelector(".clock-date").innerText = today;
     document.querySelector(".clock-time").innerText = time;
-
-};
- //カウントする
- setInterval(clock,1000);
+    //カウントする
+    setInterval(clock,1000);
+}
+ 
